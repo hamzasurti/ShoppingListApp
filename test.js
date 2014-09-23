ce0cfc3dc98e55cb91297fd2ebc252df27cb8048
hamzasurti <hamza.surti1@gmail.com>
2014-09-23 00:37:57 -0700

    start of js

---
+++ test.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('input').keyup(function(e){
+        if(e.keyCode == 13){//Enter key pressed
+		alert('mouse entered .ryu div')
+            ;});});})
+
+
+		/*var value = $(this).val();
+		$('ul').append(<li class="item">
+		    <span class="check"></span>
+		     "+text+"		      
+		    <span class= "cross"></span>  
+		</li>)
+		;})	
+	;})
+;})
